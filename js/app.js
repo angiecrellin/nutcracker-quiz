183f8cc8f90d5928c554648ec2f0b9ef215ff881
angiecrellin <angiecrellin@gmail.com>
2016-07-09 14:31:35 -0500

    css/style.css

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var questions = [
+{
+title: 'Who composed the music for The Nutcracker?',
+answers: ['Bach', 'Mozart', 'Tchaikovsky', 'Beethoven'],
+correctAnswer: [2]	
+
+/*title: 'In the first act who damages the Nutcracker?',
+answers: ['Clara', 'Fritz', 'Heir Drosselmeyer' 'The Rat King'],
+correctAnswer: [1]*/
+
+/*title: 'In the battle scene who defeats the Rat King?',
+answers: ['The Prince', 'A Toy Soldier', 'Clara', 'He is not defeated'],
+correctAnswer: [2]*/
+
+/*title: 'Who greets Clara and her prince and welcomes them to the Land of the Sweets?'
+answers: ['The Snow Queen', 'The Arabian Lead', 'The Dew Drop Fairy' 'The Sugar Plum Fairy']
+correctAnswer: [3]*/
+
+/*Title: 'Who dances the pas de deux at the end of the second act?',
+answers: ['Clara and The Prince', 'Sugar Plum Fairy and ', 'The Dolls', 'Heir Drosselmeyer and The Maid'],
+correctAnswer: [2]*/
+
+},
+
+];
+
+console.log(questions)
+
+questions.forEach(function(question) {
+  var $page = $('.question-page');
+  
+  $page.find('.title').text(question.title);
+  var $answers = $page.find('.answers');
+  question.answers.forEach(function(answer, index) {
+    $answers.append('<li><input type="radio" name="answer" value="' + index + '" />' + answer + '</li>');
+  });
+});
