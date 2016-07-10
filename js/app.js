42bba00b0f6939b3ba35de680ec9f92cff98b551
angiecrellin <angiecrellin@gmail.com>
2016-07-10 13:26:37 -0500

    -A

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 var questions = [
 {
+
 title: 'Who composed the music for The Nutcracker?',
 answers: ['Bach', 'Mozart', 'Tchaikovsky', 'Beethoven'],
-correctAnswer: [2]	
+correctAnswer: [2]
 
 /*title: 'In the first act who damages the Nutcracker?',
 answers: ['Clara', 'Fritz', 'Heir Drosselmeyer' 'The Rat King'],
@@ -12,12 +13,12 @@ correctAnswer: [1]*/
 answers: ['The Prince', 'A Toy Soldier', 'Clara', 'He is not defeated'],
 correctAnswer: [2]*/
 
-/*title: 'Who greets Clara and her prince and welcomes them to the Land of the Sweets?'
+/*title: 'Who greets Clara and The Prince and welcomes them to the Land of the Sweets?'
 answers: ['The Snow Queen', 'The Arabian Lead', 'The Dew Drop Fairy' 'The Sugar Plum Fairy']
 correctAnswer: [3]*/
 
-/*Title: 'Who dances the pas de deux at the end of the second act?',
-answers: ['Clara and The Prince', 'Sugar Plum Fairy and ', 'The Dolls', 'Heir Drosselmeyer and The Maid'],
+/*title: 'Who dances the Pas de Deux at the end of the second act?',
+answers: ['Clara and The Prince', 'Sugar Plum Fairy and The Cavalier', 'The Harlequin Doll and the Toy Soldier', 'Heir Drosselmeyer and The Maid'],
 correctAnswer: [2]*/
 
 },
