cc965bb65b1d43009582b424873d24b3acdd4b83
angiecrellin <angiecrellin@gmail.com>
2016-07-13 13:33:23 -0500

    css/style.css

--- js/app.js
+++ js/app.js
@@ -1,35 +1,39 @@
-var questions = [
-{
+var questions = [{
 
-title: 'Who composed the music for The Nutcracker?',
-answers: ['Bach', 'Mozart', 'Tchaikovsky', 'Beethoven'],
-correctAnswer: 2
-},
+        title: 'Who composed the music for The Nutcracker?',
+        answers: ['Bach', 'Mozart', 'Tchaikovsky', 'Beethoven'],
+        questionNumber: '1 of 5',
+        correctAnswer: 2
+    },
 
-{
-title: 'In the first act who damages the Nutcracker?',
-answers: ['Clara', 'Fritz', 'Heir Drosselmeyer', 'The Rat King'],
-correctAnswer: 1
-},
+    {
+        title: 'In the first act who damages the Nutcracker?',
+        answers: ['Clara', 'Fritz', 'Heir Drosselmeyer', 'The Rat King'],
+        questionNumber: '2 of 5',
+        correctAnswer: 1
+    },
 
-{
-title: 'In the battle scene who defeats the Rat King?',
-answers: ['The Prince', 'A Toy Soldier', 'Clara', 'He is not defeated'],
-correctAnswer: 2
-},
+    {
+        title: 'In the battle scene who defeats the Rat King?',
+        answers: ['The Prince', 'A Toy Soldier', 'Clara', 'He is not defeated'],
+        questionNumber: '3 of 5',
+        correctAnswer: 2
+    },
 
-{
-title: 'Who greets Clara and The Prince and welcomes them to the Land of the Sweets?',
-answers: ['The Snow Queen', 'The Arabian Lead', 'The Dew Drop Fairy', 'The Sugar Plum Fairy'],
-correctAnswer: 3
-},
+    {
+        title: 'Who greets Clara and The Prince and welcomes them to the Land of the Sweets?',
+        answers: ['The Snow Queen', 'The Arabian Lead', 'The Dew Drop Fairy', 'The Sugar Plum Fairy'],
+        questionNumber: '4 of 5',
+        correctAnswer: 3
+    },
 
-{
-title: 'Who dances the Pas de Deux at the end of the second act?',
-answers: ['Clara and The Prince', 'Sugar Plum Fairy and The Cavalier', 'The Harlequin Doll and the Toy Soldier', 'Heir Drosselmeyer and The Maid'],
-correctAnswer: 1
+    {
+        title: 'Who dances the Pas de Deux at the end of the second act?',
+        answers: ['Clara and The Prince', 'Sugar Plum Fairy and The Cavalier', 'The Harlequin Doll and the Toy Soldier', 'Heir Drosselmeyer and The Maid'],
+        questionNumber: '5 of 5',
+        correctAnswer: 1
 
-},
+    },
 
 ];
 
@@ -38,83 +42,75 @@ var currentQuestion = -1
 var totalScore = 0
 
 function nextQuestion() {
-  currentQuestion ++
-  if (currentQuestion == questions.length){
-  	showResults()
-  	return
-  }
-  var question = questions[currentQuestion]
-  var 	$page = $('.question-page');
-  		$page.find('.title').text(question.title);
-  var $answers = $page.find('.answers');
-  	$answers.empty()
-  		question.answers.forEach(function(answer, index) {
-    	$answers.append('<li><input type ="radio" name ="answer" value="' + index + '" required />' + answer + '</li>');
-  })
+    currentQuestion++
+    if (currentQuestion == questions.length) {
+        showResults()
+        return
+    }
+    var question = questions[currentQuestion]
+    var $page = $('.question-page');
+    $page.find('.title').text(question.title);
+    var $answers = $page.find('.answers');
+    $answers.empty()
+    question.answers.forEach(function(answer, index) {
+        $answers.append('<li><input type ="radio" name ="answer" value="' + index + '" required />' + answer + '</li>');
+        $page.find('.questionNumber').text(question.questionNumber);
+
+    })
 }
 
-/*function inBetweenAnswers() {
-  var question = questions[currentQuestion]
-  var guess = $('.answers').find('input:checked').val()
-      $('.results').show();
-    if (Number(guess) === question.correctAnser){
-      $('.results').text("Good Job!");
+function inBetweenAnswers() {
+    var question = questions[currentQuestion]
+    var guess = $('.answers').find('input:checked').val()
+    if (Number(guess) == question.correctAnswer) {
+        $('.results').text("Good Job!");
+    } else {
+        $('.results').text('Oops! Try Again.');
+
     }
-    else {
-      $('.results').text('Oops! Try Again.');
-    }
-  }*/
+    $('.results').show();
+    setTimeout(function() {
+        $('.results').hide();
+        nextQuestion();
 
 
+    }, 3000)
+}
+
+//show first question on page load
 nextQuestion();
 
-$('.question-page').on('submit', function(event){
-	event.preventDefault()
-	scoreQuestion();
-  //inBetweenAnswers();
-	nextQuestion();
+$('.question-page').on('submit', function(event) {
+    event.preventDefault()
+    scoreQuestion();
+    inBetweenAnswers();
+
 
 })
 
-function showResults(){
-  $('.results').find('.correct').text(totalScore)
-  $('.results').find('.total').text(questions.length)
-	$('.results').show()
+function showResults() {
 
-}
-function scoreQuestion(){
-	var question = questions[currentQuestion]
-	var guess = $('.answers').find('input:checked').val()
-	if (Number(guess) === question.correctAnswer){
-		totalScore ++
-	} 
-
+    var text = 'Congratulations you got  ' + totalScore + ' out of ' + questions.length + ' correct!<br>You\'re ready for the ballet!'
+    $('.results').show()
+    $('.results').html(text)
 
 }
 
-/*function restartQuiz() {
-  var = currentQuestion = -1
-  var = totalScore = 0
+function scoreQuestion() {
+    var question = questions[currentQuestion]
+    var guess = $('.answers').find('input:checked').val()
+    if (Number(guess) === question.correctAnswer) {
+        totalScore++
+    }
+
 
 }
-$('.restart').click(restartQuiz)*/
 
+function restartQuiz() {
+    currentQuestion = -1
+    totalScore = 0
+    $('.results').hide()
+    nextQuestion();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('.restart').click(restartQuiz)
